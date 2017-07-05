a09fdb8922b414c9a8f4c8ffa1fc3fcb1638f849
vectorzero <w3298028@163.com>
2017-07-05 13:33:47 +0800

    登录

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -7,6 +7,6 @@
 
 <<script>
 export default {
-  name:'indexPage'
+  name:'IndexPage'
 }
 </script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,9 @@
 <<template>
-    <div class='loginBox'>
+    <form class='loginBox' novalidate autocomplete="off">
         <el-input v-model='username' placeholder='请输入账号'></el-input>
         <el-input type='password' v-model='password' placeholder='请输入密码'></el-input>
         <el-button type='primary' @click='login'>登录</el-button>
-    </div>
+    </form>
 </template>
 
 <<script>
@@ -23,7 +23,11 @@ export default {
                 username:username,
                 password:password
             })) return;
-            this.$router.push({name:'indexPage'})
+            alert('登录成功');
+            this.doneLogin();
+        },
+        doneLogin() {
+            this.$router.push({name:'IndexPage'})
         },
         check(obj){
             if(obj.username===''){
